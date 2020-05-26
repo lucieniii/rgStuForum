a719c4c2dcb77b82dcb824d0957027710e3a89a3
lucieniii <lucienli0320@gmail.com>
2020-05-26 19:30:41 +0800

    Update base

---
+++ src/SE/forum/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def base(request):
+    return render(request, 'base/base.html')
+
+def post(request):
+    return render(request, 'base/post.html')
