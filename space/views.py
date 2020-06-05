87630d2e829b4380dc00178a2fb2f9769c115304
lucieniii <lucienli0320@gmail.com>
2020-06-05 16:17:15 +0800

    update post summary

--- space/views.py
+++ space/views.py
@@ -10,7 +10,8 @@ def space(request):
     posts = Post.objects.filter(author=userid)
     # 限定显示30个字符
     for i in posts:
-        i.content = i.content[0:30]
+        if len(i.content) > 30:
+            i.content = i.content[0:30] + "..."
     context = {"posts": posts}
     return render(request, "space/space.html", context)
 
