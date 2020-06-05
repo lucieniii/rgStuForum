ef07d29d0d900cb76479debe06f905902b63cb06
Jay <huaiyu11@126.com>
2020-06-05 21:03:55 +0800

    update-index3

--- forum/views.py
+++ forum/views.py
@@ -16,9 +16,23 @@ def base(request):
 
 
 # index-views
-def index(request):
+def index_old(request):
     is_login = get_login_status(request)
     return render(request, 'forum/index.html', locals())
+
+
+def index(request):
+    is_login = get_login_status(request)
+    userid = request.session.get('user_id', None)
+    posts = Post.objects.filter(author=userid)
+    # 限定显示30个字符
+    for i in posts:
+        if len(i.content) > 30:
+            i.content = i.content[0:30] + "..."
+    if posts:
+        return render(request, "forum/index.html", locals())
+    else:
+        return HttpResponse("暂无发帖记录")
 
 
 def forumBoard(request):
