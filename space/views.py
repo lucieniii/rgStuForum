ed3ccb8145aff3b7b788e25a3156505ef05b1b0f
aikx <1016194674@qq.com>
2020-06-08 23:34:15 +0800

    update space.views and space.html

--- space/views.py
+++ space/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from space.models import *
 from login.models import *
 from forum.models import *
@@ -8,52 +8,61 @@ from login.views import get_login_status
 
 # Create your views here.
 def space(request):
-    # is_login = get_login_status(request)
-    userid = request.session.get('user_id', None)
-    user = User.objects.get(id=userid)
-    posts = Post.objects.filter(author=userid)
-    comments = Comment.objects.filter(user=userid)
-    # Comment表里没有post的title属性，故在本地进行查询
-    '''
-    comments_tmp = Comment.objects.filter(user=userid)
-    comments = []
-    
-    for i in comments_tmp:
-        post = Post.objects.get(author=i.user, id=i.post)
-        title = post.title
-        author = user.name
-        tag = post.tag
-        dic = {'title': title, 'author': author, 'tag': tag}
-        comments.append(dic)
-    '''
-    # 限定显示100个字符
-    for i in posts:
-        if len(i.content) > 100:
-            i.content = i.content[0:100] + "..."
-    if posts:
+    is_login = get_login_status(request)
+    if is_login:
+        userid = request.session.get('user_id', None)
+        user = User.objects.get(id=userid)
+        posts = Post.objects.filter(author=userid)
+        comments = Comment.objects.filter(user=userid)
+        # Comment表里没有post的title属性，故在本地进行查询
+        # 发现可以直接使用comment.user访问User类，那就不需要下面的东西了
+        '''
+        comments_tmp = Comment.objects.filter(user=userid)
+        comments = []
+        
+        for i in comments_tmp:
+            post = Post.objects.get(author=i.user, id=i.post)
+            title = post.title
+            author = user.name
+            tag = post.tag
+            dic = {'title': title, 'author': author, 'tag': tag}
+            comments.append(dic)
+        '''
+        # 限定显示100个字符
+        for i in posts:
+            if len(i.content) > 100:
+                i.content = i.content[0:100] + "..."
         return render(request, "space/space.html", locals())
     else:
-        return HttpResponse("暂无发帖记录")
+        return redirect('/index/', locals())
 
 
 def settings(request):
     pass
     '''
+    # 返回个人信息
     userid = request.session.get('user_id', None)
-    follows = Follow.objects.filter(FollowerID=userid)
-    context = {"follows": follows}
-    if follows:
-        return render(request, "space/settings.html", context)
-    else:
-        return HttpResponse("暂无关注用户")
+    user = User.objects.get(id=userid)
+    return render(request, "space/settings.html", locals())
     '''
 
     '''
+    # 返回关注用户
+    userid = request.session.get('user_id', None)
+    follows = Follow.objects.filter(FollowerID=userid)
+    return render(request, "space/settings.html", locals())
+    '''
+
+    '''
+    # 返回收藏帖子
     userid = request.session.get('user_id', None)
     favoritePosts = FavoritePost.objects.filter(UserID=userid)
-    context = {"favoritePosts": favoritePosts}
-    if favoritePosts:
-        return render(request, "space/settings.html", context)
-    else:
-        return HttpResponse("暂无收藏帖子")
+    return render(request, "space/settings.html", locals())
+    '''
+
+    '''
+    # 返回黑名单
+    userid = request.session.get('user_id', None)
+    blackList = BlackList.objects.filter(BlockerID=userid)
+    return render(request, "space/settings.html", locals())
     '''
